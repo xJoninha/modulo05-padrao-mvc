887a9da3f9ea5373877538aa2ee00017ebf73809
xjoninha <jonasjfs15@gmail.com>
2022-05-05 02:17:03 -0300

    feat: resolução do desafio 3

---
+++ 03-rotas-parametrizadas/script.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const series = [
+    {
+      id: 1,
+      nome: 'Irmão do Jorel'
+    },
+    {
+      id: 2,
+      nome: 'Um maluco no pedaço'
+    }
+];
+
+app.get("/series", (req, res) =>  res.send(series))
+
+app.get("/series/:id", (req, res) =>  res.send(series.filter(serie => serie.id == req.params.id)))
+
+app.listen(3000, () => console.log("Servidor rodando na porta 3000"))
