1d5d295640c081c210f31cc182782c1f94e3f0da
xjoninha <jonasjfs15@gmail.com>
2022-05-05 01:03:09 -0300

    feat: Criando cenário do desafio

---
+++ 02-rotas-e-controller/app.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const router = require("./script.js")
+const app = express()
+const port = 3000
+
+app.use("/", router)
+
+app.listen(port, () => console.log(`Servidor rodando com sucesso na porta ${port}`))
---
+++ 02-rotas-e-controller/controllers/CarrinhoController.js
@@ -0,0 +1,9 @@
+const CarrinhoController = {
+    removeItem: function () {},
+    getItem: function () {},
+    addItem: function (req, res) {
+        res.send("Item Adicionado");
+    },
+};
+
+module.exports = CarrinhoController;
