7324ddaaef0f7d4124dfd87fed1097724e3d6f4d
xjoninha <jonasjfs15@gmail.com>
2022-05-06 22:33:30 -0300

    feat: Base do 'Express-Generator'

--- 05-express-generator/routes/index.js
+++ 05-express-generator/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'MVC', subtitle: "Aula sobre MVC, Express, Express-Generator, Node e EJS" });
 });
 
 module.exports = router;
